ca62df7b3b89e6b687935f7188a906480b59436b
NothingOW <godtes11@gmail.com>
2020-06-24 21:39:15 +0700

    Saving 'maximum' in history

--- app/src/main/java/com/example/cfberlogaapp/ResultDialog.java
+++ app/src/main/java/com/example/cfberlogaapp/ResultDialog.java
@@ -15,10 +15,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class ResultDialog extends AppCompatDialogFragment {
@@ -54,6 +60,65 @@ public class ResultDialog extends AppCompatDialogFragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         if(resultEditText.getText().toString() != ""){
+
+
+                            mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    User user  = dataSnapshot.child("users").child(mAuth.getUid()).getValue(User.class);
+                                    switch (exrsz) {
+                                        case "backSquat":
+                                            user.setBackSquat(resultEditText.getText().toString());
+                                            break;
+                                        case "frontSquat":
+                                            user.setFrontSquat(resultEditText.getText().toString());
+                                            break;
+                                        case "overheadSquat":
+                                            user.setOverheadSquat(resultEditText.getText().toString());
+                                            break;
+                                        case "deadlift":
+                                            user.setDeadlift(resultEditText.getText().toString());
+                                            break;
+                                        case "press":
+                                            user.setPress(resultEditText.getText().toString());
+                                            break;
+                                        case "benchPress":
+                                            user.setBenchPress(resultEditText.getText().toString());
+                                            break;
+                                        case "pullUps":
+                                            user.setPullUps(resultEditText.getText().toString());
+                                            break;
+                                        case "c2bPullUps":
+                                            user.setC2bPullUps(resultEditText.getText().toString());
+                                            break;
+                                        case "hsPullUps":
+                                            user.setHsPullUps(resultEditText.getText().toString());
+                                            break;
+                                        case "ringsDips":
+                                            user.setRingsDips(resultEditText.getText().toString());
+                                            break;
+                                        case "t2b":
+                                            user.setT2b(resultEditText.getText().toString());
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                    User historyUser = new User(user.getWeight(),user.getShoulders(),user.getChest(),user.getBiceps(),
+                                            user.getWaist(),user.getHip(),user.getHips(),user.getBackSquat(),user.getFrontSquat(),user.getOverheadSquat(),
+                                            user.getDeadlift(),user.getBenchPress(),user.getPress(),user.getPullUps(),user.getC2bPullUps(),user.getHsPullUps(),
+                                            user.getRingsDips(),user.getT2b());
+                                    Date currentDate = new Date();
+                                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
+                                    String currentDateText = dateFormat.format(currentDate);
+                                    mDatabase.child("history").child(mAuth.getUid()).child(currentDateText).setValue(historyUser);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+
                             mDatabase.child("users").child(mAuth.getUid()).child(exrsz).setValue(resultEditText.getText().toString());
                             Toast.makeText(getActivity(), "Информация успешно обновлена!", Toast.LENGTH_SHORT).show();
                         }
--- app/src/main/java/com/example/cfberlogaapp/User.java
+++ app/src/main/java/com/example/cfberlogaapp/User.java
@@ -77,4 +77,172 @@ public class User {
         this.ringsDips=ringsDips;
         this.t2b=t2b;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getShoulders() {
+        return shoulders;
+    }
+
+    public void setShoulders(String shoulders) {
+        this.shoulders = shoulders;
+    }
+
+    public String getChest() {
+        return chest;
+    }
+
+    public void setChest(String chest) {
+        this.chest = chest;
+    }
+
+    public String getBiceps() {
+        return biceps;
+    }
+
+    public void setBiceps(String biceps) {
+        this.biceps = biceps;
+    }
+
+    public String getWaist() {
+        return waist;
+    }
+
+    public void setWaist(String waist) {
+        this.waist = waist;
+    }
+
+    public String getHip() {
+        return hip;
+    }
+
+    public void setHip(String hip) {
+        this.hip = hip;
+    }
+
+    public String getHips() {
+        return hips;
+    }
+
+    public void setHips(String hips) {
+        this.hips = hips;
+    }
+
+    public String getBackSquat() {
+        return backSquat;
+    }
+
+    public void setBackSquat(String backSquat) {
+        this.backSquat = backSquat;
+    }
+
+    public String getFrontSquat() {
+        return frontSquat;
+    }
+
+    public void setFrontSquat(String frontSquat) {
+        this.frontSquat = frontSquat;
+    }
+
+    public String getOverheadSquat() {
+        return overheadSquat;
+    }
+
+    public void setOverheadSquat(String overheadSquat) {
+        this.overheadSquat = overheadSquat;
+    }
+
+    public String getDeadlift() {
+        return deadlift;
+    }
+
+    public void setDeadlift(String deadlift) {
+        this.deadlift = deadlift;
+    }
+
+    public String getBenchPress() {
+        return benchPress;
+    }
+
+    public void setBenchPress(String benchPress) {
+        this.benchPress = benchPress;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+    public String getPullUps() {
+        return pullUps;
+    }
+
+    public void setPullUps(String pullUps) {
+        this.pullUps = pullUps;
+    }
+
+    public String getC2bPullUps() {
+        return c2bPullUps;
+    }
+
+    public void setC2bPullUps(String c2bPullUps) {
+        this.c2bPullUps = c2bPullUps;
+    }
+
+    public String getHsPullUps() {
+        return hsPullUps;
+    }
+
+    public void setHsPullUps(String hsPullUps) {
+        this.hsPullUps = hsPullUps;
+    }
+
+    public String getRingsDips() {
+        return ringsDips;
+    }
+
+    public void setRingsDips(String ringsDips) {
+        this.ringsDips = ringsDips;
+    }
+
+    public String getT2b() {
+        return t2b;
+    }
+
+    public void setT2b(String t2b) {
+        this.t2b = t2b;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
 }
